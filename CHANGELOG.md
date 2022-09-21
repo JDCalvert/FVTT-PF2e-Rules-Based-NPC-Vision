b6da6aca100302d8df3ead973c1d1b2a22d54032
Jonathan Calvert <jd.calvert@hotmail.co.uk>
2022-09-21 23:14:50 +0100

    Foundry v10 Compatibility

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 2.0.0 - 2022-09-21
+- Foundry v10 compatibility
+
 ## 1.0.2 - 2022-05-06
 - Fix an issue with NPCs with missing sense data
 
--- scripts/npcvision.js
+++ scripts/npcvision.js
@@ -1,5 +1,3 @@
-const pf2eRulesBasedNpcVisionModuleName = "pf2e-rules-based-npc-vision";
-
 /** Equivalent to the VisionLevel enum in the Pathfinder 2e system */
 class VisionLevelPF2e {
     static BLINDED = 0;
@@ -14,10 +12,10 @@ Hooks.on(
         // Calculate the NPCs vision level from its sense field. The field is a free-text string, so we have to do a bit of
         // maniplulation and make some assumptions about the data.
         libWrapper.register(
-            pf2eRulesBasedNpcVisionModuleName,
+            "pf2e-rules-based-npc-vision",
             "CONFIG.PF2E.Actor.documentClasses.npc.prototype.visionLevel",
             function npcVisionLevel() {
-                const senses = (this.data.data.traits.senses.value ?? "").split(",").map(s => s.replace(/[\s-]+/g, '').toLowerCase());
+                const senses = (this.system.traits.senses.value ?? "").split(",").map(s => s.replace(/[\s-]+/g, '').toLowerCase());
                 return this.getCondition("blinded") ? VisionLevelPF2e.BLINDED
                     : senses.some(sense => ["darkvision", "greaterdarkvision"].includes(sense))
                         ? VisionLevelPF2e.DARKVISION
@@ -28,46 +26,13 @@ Hooks.on(
             "OVERRIDE"
         );
 
-        // The Pathfinder 2e system and Perfect Vision both ignore NPCs for vision settings. Since the Perfect Vision
-        // code effectively overrides the base system code, we'll change how we override depending on the module's presence.
-        if (game.modules.get("perfect-vision")?.active) {
-            libWrapper.register(
-                pf2eRulesBasedNpcVisionModuleName,
-                "CONFIG.Token.documentClass.prototype.prepareDerivedData",
-                function prepareNpcDerivedData(wrapped, ...args) {
-                    wrapped(args);
-
-                    if (!(this.initialized && this.actor && this.scene)) return;
-
-                    if (this.scene.rulesBasedVision && this.actor.type === "npc") {
-                        this.data.dimSight = this.data._source.dimSight = this.hasLowLightVision ? 10000 : 0;
-                        this.data.brightSight = this.data._source.brightSight = this.hasDarkvision ? 10000 : 0;
-
-                        const isBlinded = this.actor.visionLevel === 0;
-
-                        setProperty(this.data, "flags.perfect-vision.sightLimit", isBlinded ? 0 : null);
-                        setProperty(this.data._source, "flags.perfect-vision.sightLimit", isBlinded ? 0 : null);
-                    }
-                },
-                "WRAPPER"
-            );
-        } else {
-            libWrapper.register(
-                pf2eRulesBasedNpcVisionModuleName,
-                "CONFIG.Token.documentClass.prototype.prepareDerivedData",
-                function prepareNpcDerivedData(wrapped, ...args) {
-                    wrapped(args);
-
-                    if (!(this.initialized && this.actor && this.scene)) return;
-
-                    if (this.scene.rulesBasedVision && this.actor.type === "npc") {
-                        const hasDarkvision = this.hasDarkvision && (this.scene.isDark || this.scene.isDimlyLit);
-                        const hasLowLightVision = (this.hasLowLightVision || this.hasDarkvision) && this.scene.isDimlyLit;
-                        this.data.brightSight = this.data._source.brightSight = hasDarkvision || hasLowLightVision ? 1000 : 0;
-                    }
-                },
-                "WRAPPER"
-            );
-        }
+        libWrapper.register(
+            "pf2e-rules-based-npc-vision",
+            "CONFIG.Token.documentClass.prototype.rulesBasedVision",
+            function npcRulesBasedVision() {
+                return !!(this.sight.enabled && this.actor?.isOfType("character", "familiar", "npc") && this.scene?.rulesBasedVision);
+            },
+            "OVERRIDE"
+        );
     }
 );
